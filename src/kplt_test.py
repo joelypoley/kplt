74072076ec8ad2da735f3618a722954082782267
Joel Laity <joel.laity@gmail.com>
2018-05-18 14:07:23 +1200

    Completed up to Step 4

--- src/kplt_test.py
+++ src/kplt_test.py
@@ -5,7 +5,8 @@ import unittest
 from sage.all import *
 from kplt import prime_norm_representative
 from kplt import element_of_norm
-
+from kplt import find_generators
+from kplt import left_ideal
 
 class IdealsTest(unittest.TestCase):
 
@@ -15,6 +16,14 @@ class IdealsTest(unittest.TestCase):
         I = O.left_ideal(O.basis()).scale(2)
         J = prime_norm_representative(I, O, 4, 2)
         self.assertTrue(J.norm() in Primes() and J.left_order() == O)
+
+    def test_find_generator(self):
+        B = QuaternionAlgebra(59)
+        O = B.maximal_order()
+        I = O.left_ideal([2*x for x in O.basis()])
+        N, alpha = find_generators(I)
+        J = left_ideal([N, alpha], O)
+        self.assertTrue(J == I)
 
     def test_element_of_norm(self):
         B = QuaternionAlgebra(59)
